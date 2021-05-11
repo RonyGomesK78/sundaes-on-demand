a72bb20ed6514e0d5cb202482b812d11e81772eb
Rony78 <gromilton7@gmail.com>
2021-05-11 14:58:29 -0100

    code and test the popover

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,27 +1,56 @@
-import { render, fireEvent, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "../SummaryForm";
 
-test('initial conditions', () => {
-    render(<SummaryForm />);
+test("initial conditions", () => {
+  render(<SummaryForm />);
 
-    const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i } );
-    expect(checkbox).not.toBeChecked();
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
 
-    const button = screen.getByRole("button", { name: /confirm order/i } );
-    expect(button).toBeDisabled();
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  expect(button).toBeDisabled();
 });
 
-test('Checkbox enables button on first click and disables on second click', () => {
-    render(<SummaryForm />)
+test("Checkbox enables button on first click and disables on second click", () => {
+  render(<SummaryForm />);
 
-    const button = screen.getByRole("button", { name: /confirm order/i } )
-    const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i } );
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
 
-    fireEvent.click(checkbox);
-    expect(button).toBeEnabled();
+  userEvent.click(checkbox);
+  expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
-    expect(button).toBeDisabled();
+  userEvent.click(checkbox);
+  expect(button).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  //popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved( () => screen.queryByText(
+    /no ice cream will actually be delivered/i
+  ));
+
 });
