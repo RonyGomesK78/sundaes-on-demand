a0bf207248d59a32650a39b91d69bd5f5b3f0f2f
Rony78 <gromilton7@gmail.com>
2021-05-12 17:53:54 -0100

    Test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,35 +1,35 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import ScoopOption from './ScoopOption';
-import ToppingOption from './ToppingOption';
-import Row from 'react-bootstrap/Row';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
-    const [ items, setItems ] = useState([]);
+  //optionType is 'scope' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => setError(true));
+  }, [optionType]);
 
-    //optionType is 'scope' or 'toppings'
-    useEffect(() => {
-        axios
-            .get(`http://localhost:3030/${optionType}`)
-            .then( response => setItems(response.data))
-            .catch((error) => {
-                // TODO: handle error response
-            });
-    }, [optionType]);
+  if (error) {
+    return <AlertBanner />;
+  }
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
-    const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
-
-    const optionItems = items.map( item => (
-        <ItemComponent 
-            key={item.name}
-            name={item.name}
-            imagePath={item.imagePath} 
-        />
-    ));
-    return(
-        <Row>{optionItems}</Row>
-    );
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+  return <Row>{optionItems}</Row>;
 };
 
-export default Options; 
+export default Options;
